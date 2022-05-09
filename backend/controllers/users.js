7ddac670f07db77c7a0703e03da17c0e6616a10b
idmx <slavyan1507@mail.ru>
2022-05-09 14:39:52 +0300

    Fix: cookie domain

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -149,7 +149,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         sameSite: 'none',
-        domain: "nomoredomains.xyz"
+        domain: ".nomoredomains.xyz"
       });
       res.send({ _id: user._id });
     })
