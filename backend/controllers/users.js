0cf012a6c07863efd87c58f6d9e20fef34ac6e94
idmx <slavyan1507@mail.ru>
2022-05-09 13:14:04 +0300

    Add: httpOnly

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -148,6 +148,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
+        httpOnly: true
       });
       res.send({ _id: user._id });
     })
