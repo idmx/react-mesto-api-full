df1bac61224029c18eb34ed6e21cd2cdec2ac1d2
idmx <slavyan1507@mail.ru>
2022-05-09 14:24:45 +0300

    Remove: secure

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -148,8 +148,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
-        sameSite: 'none',
-        secure: true
+        sameSite: 'none'
       });
       res.send({ _id: user._id });
     })
